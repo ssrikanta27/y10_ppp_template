0091c1561583c469ec5d42411527af0274b29737
ssrikanta27 <ssrikanta27@kellettschool.com>
2025-08-31 13:41:14 +0800

    creating the Deck

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ colors = ['Red', 'Green', 'Blue', 'Yellow']
 values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
 wildCards = ['Wild', 'Wild Draw Four']
 
+def createDeck():
+    deck = []
+    for color in colors:
+        for value in values:
+            deck.append((color, value))
+            if value != '0':
+                deck.append((color, value)) 
+    for wild in wildCards:
+        for _ in range(4):
+            deck.append(('Black', wild))
+    random.shuffle(deck)
+    return deck
