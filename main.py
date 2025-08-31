d1bf8352e7d20d020642470092cde5a297261231
ssrikanta27 <ssrikanta27@kellettschool.com>
2025-08-31 13:39:54 +0800

    Set Up Variables

---
+++ main.py
@@ -0,0 +1,12 @@
+# Single Player UNO Game
+import random
+
+colors = ['Red', 'Green', 'Blue', 'Yellow']
+values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
+wildCards = ['Wild', 'Wild Draw Four']
+
+players = []
+colors = ['Red', 'Green', 'Blue', 'Yellow']
+values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
+wildCards = ['Wild', 'Wild Draw Four']
+
