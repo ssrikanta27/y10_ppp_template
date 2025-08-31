6125c05113085d38cb8925f9421233f22ce5dad8
ssrikanta27 <ssrikanta27@kellettschool.com>
2025-08-31 13:47:57 +0800

    Starting the game + small edits to the setup

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-# Single Player UNO Game
 import random
 
+# Set Up
 colors = ['Red', 'Green', 'Blue', 'Yellow']
 values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
 wildCards = ['Wild', 'Wild Draw Four']
 
 players = []
-colors = ['Red', 'Green', 'Blue', 'Yellow']
-values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
-wildCards = ['Wild', 'Wild Draw Four']
+hands = []
+discardPile = []
+drawPile = []
+currentPlayer = 0
+playDirection = 1
 
+# Setup Functions
 def createDeck():
     deck = []
     for color in colors:
@@ -29,3 +32,12 @@ def dealCards(deck, numPlayers):
         for i in range(numPlayers):
             playerHands[i].append(deck.pop())
     return playerHands
+
+def initializeGame(numPlayers):
+    global players, hands, drawPile, discardPile, currentPlayer, playDirection # You Could pass these as parameters but the code is slightly easier to read this way
+    players = ['You'] + [f'Bot {i}' for i in range(1, numPlayers)] # Sets the different player (Not their cards)
+    drawPile = createDeck()
+    hands = dealCards(drawPile, numPlayers) # Sets their cards
+    discardPile.append(drawPile.pop()) 
+    currentPlayer = 0
+    playDirection = 1
